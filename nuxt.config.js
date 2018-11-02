5cbffa177e5efcc8ef1037eebedd8a25b9963f3d
Chris Zubak-Skees <chriszs@gmail.com>
2018-11-02 18:35:51 -0400

    Turn off eslint loader in prod

--- nuxt.config.js
+++ nuxt.config.js
@@ -88,7 +88,7 @@ module.exports = {
                 exclude: /(node_modules)/
             });
 
-            if (ctx.isClient) {
+            if (ctx.isClient && process.env.NODE_ENV !== 'production') {
                 config.module.rules.push({
                     enforce: 'pre',
                     test: /\.(js|vue)$/,
